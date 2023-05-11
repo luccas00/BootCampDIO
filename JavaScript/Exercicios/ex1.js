0ad73e5b3bfdb083f5af1f3cf134ac1701245a5a
luccas00 <luccas.carneiro@hotmail.com>
2023-05-11 17:43:50 -0300

    Novas Aulas BootCamp

---
+++ JavaScript/Exercicios/ex1.js
@@ -0,0 +1,17 @@
+console.log('Inicio do Log');
+
+let notas = [];
+
+notas[0] = 7;
+notas[1] = 7;
+notas[2] = 5;
+
+const media = notas[0]+notas[1]+notas[2]/3;
+
+console.log(notas[0]);
+
+
+
+
+
+console.log('Fim do Log');
